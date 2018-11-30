b8c574471f449e9ed7a82fc2970dc473f5ccfdbe
Julian Keller <julian.keller@wsu.edu>
2018-11-30 11:10:07 -0800

    Saving progress

--- budget/imports/ui/pages/home/home.js
+++ budget/imports/ui/pages/home/home.js
@@ -3,6 +3,8 @@ import './home.html';
 import '../../layouts/header/header.html';
 import '../../layouts/footer/footer.html';
 
+import '../../pages/budget/budget.html';
+
 import '../../components/home-body/home-body.html';
 
 import '../../components/hello/hello.js';
