1a3f4f93b111123b33946e5b0f7cbaf7c5acaefc
Julian Keller <julian.keller@wsu.edu>
2018-11-30 17:44:00 -0800

    starting with friends app as foundation

---
+++ betterBudget/app/imports/api/stuff/stuff.js
@@ -0,0 +1,64 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+/* eslint-disable object-shorthand */
+
+export const Stuff = new Mongo.Collection('Stuff');
+
+/**
+ * Create the schema for Stuff
+ */
+export const StuffSchema = new SimpleSchema({
+  first: {
+    label: 'First',
+    type: String,
+    optional: false,
+    max: 20,
+    autoform: {
+      group: 'Add Contact',
+      placeholder: 'First Name',
+    },
+  },
+  last: {
+    label: 'Last',
+    type: String,
+    optional: false,
+    max: 20,
+    autoform: {
+      group: 'Add Contact',
+      placeholder: 'Last Name',
+    },
+  },
+  address: {
+    label: 'Address',
+    type: String,
+    optional: false,
+    max: 30,
+    autoform: {
+      group: 'Add Contact',
+      placeholder: 'Address',
+    },
+  },
+  phone: {
+    label: 'Telephone',
+    type: String,
+    optional: false,
+    max: 20,
+    autoform: {
+      group: 'Add Contact',
+      placeholder: 'Telephone',
+    },
+  },
+  email: {
+    label: 'Email',
+    type: String,
+    optional: false,
+    max: 30,
+    autoform: {
+      group: 'Add Contact',
+      placeholder: 'Email',
+    },
+  },
+});
+
+Stuff.attachSchema(StuffSchema);
---
+++ betterBudget/app/imports/ui/layouts/index.js
@@ -0,0 +1,7 @@
+import './app-body.html';
+import './app-not-found.html';
+import './footer.html';
+import './header.html';
+import './if-logged-in.html';
+import './if-logged-in.js';
+import './loading.html';
