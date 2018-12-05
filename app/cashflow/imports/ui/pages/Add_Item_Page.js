f23e7c5743b71e12d5cc4cc13f2cb843579d96ca
Julian Keller <julian.keller@wsu.edu>
2018-12-04 16:02:18 -0800

    adding up budget values works, need to display result

--- app/cashflow/imports/ui/pages/Add_Item_Page.js
+++ app/cashflow/imports/ui/pages/Add_Item_Page.js
@@ -2,6 +2,8 @@ import { AutoForm } from 'meteor/aldeed:autoform';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
 import { Stuff } from '../../api/stuff/stuff.js';
+import { Summary } from '../../api/stuff/summary.js';
+import { Mongo } from 'meteor/mongo';
 
 /* eslint-disable object-shorthand, no-unused-vars */
 
@@ -19,6 +21,28 @@ AutoForm.hooks({
     // onSuccess: function onSuccess(formType, result) {
     //   FlowRouter.go('Budget_Page');
     // },
+    onSuccess: function onSuccess(formType, result) {
+      // add an item to the summary schema if the database is empty
+      if (Summary.find().count() === 0) {
+        Summary.insert({ total: 0, available: 0, addBalance: 0 });
+      }
+      // aggregate the stuff data
+      const values = Stuff.find().map(function (doc) {
+        return doc.balance;
+      });
+      let sum = 0;
+      values.forEach(function (element) {
+        sum += Number(element);
+      });
+
+      Summary.update(
+          { total: { label: 'Total' } },
+          { $set: { total: sum } },
+      );
+      // console.log(values);
+      // update the summary schema
+      FlowRouter.go('Budget_Page');
+    },
   },
 });
 
