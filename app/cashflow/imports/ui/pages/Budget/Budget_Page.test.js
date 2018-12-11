7fdea79cdca7b15695b128d953c644e4083ff9de
Julian <18487101+JulianKeller@users.noreply.github.com>
2018-12-10 19:10:11 -0800

    Update Budget_Page.test.js

--- app/cashflow/imports/ui/pages/Budget/Budget_Page.test.js
+++ app/cashflow/imports/ui/pages/Budget/Budget_Page.test.js
@@ -24,17 +24,7 @@ import { assert } from 'meteor/practicalmeteor:chai';
 // Test helpers with Template.Budget_Page.__helpers[' difference']();
 // Just a simple test to test that testing is working
 describe('Budget Page', function () {
-  // test adding an expense
   it('This test passes!', function () {
     assert.equal(Stuff.find().count(), 0);
   });
 });
-
-
-// Test that the expense belongs to the current user
-
-// Test removing an expense item
-
-// Test adding a goal item
-
-// Test removing a goal item
