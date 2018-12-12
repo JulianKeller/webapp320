081372b21b0d4ee4397f118f033451ee02d9f127
Tyler Higgins <tyler.higgins@wsu.edu>
2018-12-12 02:26:31 -0800

    Added support schema
    I have a support schema (support.js), only issue is it wont submit, once we can submit, I will redirect to a page other than submitQ.

---
+++ app/cashflow/imports/api/stuff/support.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+/* eslint-disable object-shorthand */
+
+export const Support = new Mongo.Collection('Support');
+
+export const SupportSchema = new SimpleSchema({
+  issue: {
+    type: String,
+    optional: false,
+    label: 'Issue:',
+    autoform: {
+      group: 'Support',
+      placeholder: 'Select Issue'
+    },
+  },
+
+  details: {
+    label: 'Details',
+    type: String,
+    optional: false,
+    max: 250,
+    autoform: {
+      group: 'Support',
+      placeholder: 'Details here...',
+      rows: 10,
+    },
+  },
+});
+
+Support.attachSchema(SupportSchema);
--- app/cashflow/imports/ui/pages/Support/Faq_Page.js
+++ app/cashflow/imports/ui/pages/Support/Faq_Page.js
@@ -2,9 +2,15 @@ import { AutoForm } from 'meteor/aldeed:autoform';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
 import { Question } from '../../../api/stuff/question.js';
+import { Support } from '../../../api/stuff/support.js';
 
 AutoForm.hooks({
   AddQuestionForm: {
+    onSuccess: function onSuccess(formType, result) {
+      FlowRouter.go('SubmitQ_Page');
+    },
+  },
+  AddSupportForm: {
     onSuccess: function onSuccess(formType, result) {
       FlowRouter.go('SubmitQ_Page');
     },
@@ -17,7 +23,20 @@ Template.Faq_Page.onRendered(function () {
 });
 
 Template.Faq_Page.helpers({
+  options(){
+    return {
+      no_display: 'My budget(s) are not displaying correctly.',
+      no_add: 'I cannot add a new item/goal.',
+      no_update_bal: 'My balance is not updating correctly.',
+      no_update_info: 'I\'m having trouble updating my information.',
+      delete_acct: 'I\'d like to delete my account.',
+      other: 'Other (Please describe below)'
+    };
+  },
   questionCollection() {
     return Question;
+  },
+  supportCollection() {
+    return Support;
   },
 });
